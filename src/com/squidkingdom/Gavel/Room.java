50fe2299f0ce99ad586bd6ef3a0112892876737b
Brady <oskiekid@gmail.com>
2020-03-18 15:36:42 -0500

    Auto stash before merge of "master" and "origin/jacob-development"

---
+++ src/com/squidkingdom/Gavel/Room.java
@@ -0,0 +1,5 @@
+package com.squidkingdom.Gavel;
+
+public class Room {
+
+}
---
+++ src/com/squidkingdom/Gavel/RoomManager.java
@@ -0,0 +1,9 @@
+package com.squidkingdom.Gavel;
+
+public class RoomManager {
+    public static Room[] roomArray = new Room[11];
+
+
+    public RoomManager
+
+}
