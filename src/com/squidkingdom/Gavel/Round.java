764e29eb4185d2114494f4f4a37db90f782d8823
unroasted <jacobvm04@gmail.com>
2020-03-18 16:35:47 -0400

    Working on the round class

--- src/com/squidkingdom/Gavel/Round.java
+++ src/com/squidkingdom/Gavel/Round.java
@@ -2,9 +2,18 @@ package com.squidkingdom.Gavel;
 
 public class Round {
     boolean didWin;
+    boolean side;
     Team oppTeam = new Team();
     int affSpeaks = 0;
     int negSpeaks = 0;
     Judge judge = new Judge();
 
+    Round(boolean didWin, boolean side, Team oppTeam, int affSpeaks, int negSpeaks, Judge judge) {
+        this.didWin = didWin;
+        this.side = side;
+        this.oppTeam = oppTeam;
+        this.affSpeaks = affSpeaks;
+        this.negSpeaks = negSpeaks;
+        this.judge = judge;
+    }
 }
