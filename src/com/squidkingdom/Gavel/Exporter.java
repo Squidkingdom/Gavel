f5868c7fe3f409160e2aa16d371b1d6478d44b70
unroasted <jacobvm04@gmail.com>
2020-03-24 20:32:53 -0400

    Export schedule

--- src/com/squidkingdom/Gavel/Exporter.java
+++ src/com/squidkingdom/Gavel/Exporter.java
@@ -1,32 +1,28 @@
-//package com.squidkingdom.Gavel;
-//
-//import java.io.FileWriter;
-//import java.io.IOException;
-//
-//public class Exporter {
-//
-//    public static void exportRound(Round round, String filename) throws IOException {
-//        FileWriter csvWriter = new FileWriter("new.csv");
-//        csvWriter.append("Winner");
-//        csvWriter.append(",");
-//        csvWriter.append("Aff Team");
-//        csvWriter.append(",");
-//        csvWriter.append("Aff Speaks");
-//        csvWriter.append(",");
-//        csvWriter.append("Neg Team");
-//        csvWriter.append(",");
-//        csvWriter.append("Neg Speaks");
-//        csvWriter.append(",");
-//        csvWriter.append("Judge");
-//        csvWriter.append("\n");
-//
-//        String winner = round.didWin ? "Aff" : "Neg";
-//
-//        // Need both teams in order to export this data
-//        csvWriter.append(String.join(",", winner, round.oppTeam.person1 + " + " +  round.affTeam.person2, Integer.toString(round.affSpeaks), round.negTeam.person1 + " + " +  round.negTeam.person2, Integer.toString(round.negSpeaks), round.judge.name));
-//        csvWriter.append("\n");
-//
-//        csvWriter.flush();
-//        csvWriter.close();
-//    }
-//}
+package com.squidkingdom.Gavel;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Exporter {
+
+    public static void exportSchedule(ArrayList<RoundData> rounds) throws IOException {
+        FileWriter csvWriter = new FileWriter("schedule.csv");
+        csvWriter.append("Aff Team");
+        csvWriter.append(",");
+        csvWriter.append("Neg Team");
+        csvWriter.append(",");
+        csvWriter.append("Judge");
+        csvWriter.append(",");
+        csvWriter.append("Room");
+        csvWriter.append("\n");
+
+        for (RoundData round : rounds) {
+            csvWriter.append(String.join(",", round.affTeam.code, round.negTeam.code, round.judge.code, Integer.toString(round.room)));
+        }
+        csvWriter.append("\n");
+
+        csvWriter.flush();
+        csvWriter.close();
+    }
+}
